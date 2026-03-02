029b3a5dc000e18affa9919471d63fb783e920ef
MinhQuang <minhquanghmqno1@gmail.com>
2026-03-02 07:42:44 +0700

    connect db

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import app from "./server.ts";
+import app from "@/server.js";
+import dotenv from 'dotenv';
+import { connectDB } from "@/config/db.js";
+import express from "express";
+dotenv.config();
+
+connectDB();
+app.use(express.json());
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
