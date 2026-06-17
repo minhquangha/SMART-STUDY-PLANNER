319c6772d3cabfa87507ee3ca64263afdea361b2
MinhQuang <minhquanghmqno1@gmail.com>
2026-06-18 06:49:44 +0700

    adjust environment

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { globalErrorHandler } from '@/middlewares/global_error.js'
 import publicRoutes from '@/api/public/index.js'
 import meRoute from '@/api/me/index.js'
 import adminRoutes from '@/api/admin/index.js'
-import usersRoutes from '@/api/users/index.js'
 import{ authMiddlewares} from '@/middlewares/authmiddlewares.js'
 import { requireRole } from '@/middlewares/admin.js'
 import cors from 'cors'
@@ -16,18 +15,19 @@ dotenv.config();
 connectDB();
 const app:Application = express()
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }))
 app.use(cookieParser())
 app.use(express.json())
 app.use("/",publicRoutes);
 app.use("/me",authMiddlewares,meRoute);
-app.use("/api/users", usersRoutes);
 app.use("/admin", authMiddlewares, requireRole("admin"), adminRoutes);
 app.use(globalErrorHandler);
 
-app.listen(3000, () => {
+
+const PORT = process.env.PORT||3000
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
   startDeadlineReminder();
 });
